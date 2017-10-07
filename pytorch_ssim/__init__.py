df1c2f26e63aca31cea1725120f110b8bbf43469
Evan Su <evan.pohsun.su@gmail.com>
2017-10-07 12:23:50 -0400

    Force int division

--- pytorch_ssim/__init__.py
+++ pytorch_ssim/__init__.py
@@ -5,7 +5,7 @@ import numpy as np
 from math import exp
 
 def gaussian(window_size, sigma):
-    gauss = torch.Tensor([exp(-(x - window_size/2)**2/float(2*sigma**2)) for x in range(window_size)])
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
     return gauss/gauss.sum()
 
 def create_window(window_size, channel):
@@ -15,16 +15,16 @@ def create_window(window_size, channel):
     return window
 
 def _ssim(img1, img2, window, window_size, channel, size_average = True):
-    mu1 = F.conv2d(img1, window, padding = window_size/2, groups = channel)
-    mu2 = F.conv2d(img2, window, padding = window_size/2, groups = channel)
+    mu1 = F.conv2d(img1, window, padding = window_size//2, groups = channel)
+    mu2 = F.conv2d(img2, window, padding = window_size//2, groups = channel)
 
     mu1_sq = mu1.pow(2)
     mu2_sq = mu2.pow(2)
     mu1_mu2 = mu1*mu2
 
-    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size/2, groups = channel) - mu1_sq
-    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size/2, groups = channel) - mu2_sq
-    sigma12 = F.conv2d(img1*img2, window, padding = window_size/2, groups = channel) - mu1_mu2
+    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size//2, groups = channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size//2, groups = channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding = window_size//2, groups = channel) - mu1_mu2
 
     C1 = 0.01**2
     C2 = 0.03**2
