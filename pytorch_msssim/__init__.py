4182e57b31b11ab34da11175a45e75558300a543
jorge-pessoa <jpessoa.on@gmail.com>
2018-07-20 15:31:45 +0100

    Moved changes to correct file

--- pytorch_msssim/__init__.py
+++ pytorch_msssim/__init__.py
@@ -1,135 +1,109 @@
 import torch
 import torch.nn.functional as F
-from torch.autograd import Variable
-import numpy as np
 from math import exp
-
+import numpy as np
 
 def gaussian(window_size, sigma):
     gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    # gauss.requires_grad = True
     return gauss/gauss.sum()
 
 
-def create_window(window_size, channel):
+def create_window(window_size):
     _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    window = _2D_window.expand(1, 1, window_size, window_size).contiguous()
     return window
 
 
-def _ssim(img1, img2, window, window_size, channel, size_average=True, full=False):
-    padd = 0
+def ssim(img1, img2, window_size=11, size_average=True, full=False, val_range=None):
+    # Value range can be different from 255. Other common ranges are 1 (sigmoid) and 2 (tanh).
+    if val_range is None:
+        if torch.max(img1) > 128:
+            max_val = 255
+        else:
+            max_val = 1
 
-    mu1 = F.conv2d(img1, window, padding=padd, groups=channel)
-    mu2 = F.conv2d(img2, window, padding=padd, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1*mu2
-
-    sigma1_sq = F.conv2d(img1*img1, window, padding=padd, groups=channel) - mu1_sq
-    sigma2_sq = F.conv2d(img2*img2, window, padding=padd, groups=channel) - mu2_sq
-    sigma12 = F.conv2d(img1*img2, window, padding=padd, groups=channel) - mu1_mu2
-
-    C1 = 0.01**2
-    C2 = 0.03**2
-
-    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
-
-    v1 = 2.0 * sigma12 + C2
-    v2 = sigma1_sq + sigma2_sq + C2
-    cs = torch.mean(v1 / v2)
-
-    if size_average:
-        ret = ssim_map.mean()
+        if torch.min(img1) < -0.5:
+            min_val = -1
+        else:
+            min_val = 0
+        L = max_val - min_val
     else:
-        ret = ssim_map.mean(1).mean(1).mean(1)
+        L = val_range
+
+    padd = 0
+    (_, channels, height, width) = img1.size()
+    real_size = min(window_size, height, width)
+    window = create_window(real_size)
+
+    ret_channels = []
+    cs_channels = []
+    for ch in range(channels):  # loop over channels, then average
+        img1_ch = torch.unsqueeze(img1[:, ch, :, :], 1)
+        img2_ch = torch.unsqueeze(img2[:, ch, :, :], 1)
+        mu1 = F.conv2d(img1_ch, window, padding=padd)
+        mu2 = F.conv2d(img2_ch, window, padding=padd)
+
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+
+        sigma1_sq = F.conv2d(img1_ch * img1_ch, window, padding=padd) - mu1_sq
+        sigma2_sq = F.conv2d(img2_ch * img2_ch, window, padding=padd) - mu2_sq
+        sigma12 = F.conv2d(img1_ch * img2_ch, window, padding=padd) - mu1_mu2
+
+        C1 = (0.01 * L) ** 2
+        C2 = (0.03 * L) ** 2
+
+        ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+        v1 = 2.0 * sigma12 + C2
+        v2 = sigma1_sq + sigma2_sq + C2
+        cs = torch.mean(v1 / v2)  # contrast sensitivity
+
+        if size_average:
+            ret = ssim_map.mean()
+        else:
+            ret = ssim_map.mean(1).mean(1).mean(1)
+
+        cs_channels.append(cs)
+        ret_channels.append(ret)
+
+    cs_mean = torch.mean(torch.stack(cs_channels), dim=-1)
+    ret_mean = torch.mean(torch.stack(ret_channels), dim=-1)
 
     if full:
-        return ret, cs
-    return ret
+        return ret_mean, cs_mean
+    return ret_mean
 
 
-class SSIM(torch.nn.Module):
-    def __init__(self, window_size=11, size_average=True):
-        super(SSIM, self).__init__()
-        self.window_size = window_size
-        self.size_average = size_average
-        self.channel = 1
-        self.window = create_window(window_size, self.channel)
-
-    def forward(self, img1, img2):
-        (_, channel, _, _) = img1.size()
-
-        if channel == self.channel and self.window.data.type() == img1.data.type():
-            window = self.window
-        else:
-            window = create_window(self.window_size, channel)
-            
-            if img1.is_cuda:
-                window = window.cuda(img1.get_device())
-            window = window.type_as(img1)
-            
-            self.window = window
-            self.channel = channel
-
-        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
-
-
-def ssim(img1, img2, window_size=11, size_average=True, full=False):
-    (_, channel, height, width) = img1.size()
-
-    real_size = min(window_size, height, width)
-    window = create_window(real_size, channel)
-    
-    if img1.is_cuda:
-        window = window.cuda(img1.get_device())
-    window = window.type_as(img1)
-    
-    return _ssim(img1, img2, window, real_size, channel, size_average, full=full)
-
-
-def msssim(img1, img2, window_size=11, size_average=True):
-    # TODO: fix NAN results
-    if img1.size() != img2.size():
-        raise RuntimeError('Input images must have the same shape (%s vs. %s).' %
-                           (img1.size(), img2.size()))
-    if len(img1.size()) != 4:
-        raise RuntimeError('Input images must have four dimensions, not %d' %
-                           len(img1.size()))
-
-    if type(img1) is not Variable or type(img2) is not Variable:
-        raise RuntimeError('Input images must be Variables, not %s' % 
-                            img1.__class__.__name__)
-
-    weights = Variable(torch.FloatTensor([0.0448, 0.2856, 0.3001, 0.2363, 0.1333]))
-    if img1.is_cuda:
-        weights = weights.cuda(img1.get_device())
-
+def msssim(img1, img2, window_size=11, size_average=True, val_range=None):
+    device = img1.device
+    weights = torch.FloatTensor([0.0448, 0.2856, 0.3001, 0.2363, 0.1333]).to(device)
     levels = weights.size()[0]
     mssim = []
     mcs = []
     for _ in range(levels):
-        sim, cs = ssim(img1, img2, window_size=window_size, size_average=size_average, full=True)
+        sim, cs = ssim(img1, img2, window_size=window_size, size_average=size_average, full=True, val_range=val_range)
         mssim.append(sim)
         mcs.append(cs)
 
         img1 = F.avg_pool2d(img1, (2, 2))
         img2 = F.avg_pool2d(img2, (2, 2))
 
-    mssim = torch.cat(mssim)
-    mcs = torch.cat(mcs)
-    return (torch.prod(mcs[0:levels-1] ** weights[0:levels-1]) *
-            (mssim[levels-1] ** weights[levels-1]))
+    mssim = torch.stack(mssim)
+    mcs = torch.stack(mcs)
 
+    # # Normalize (to avoid NaNs)
+    #
+    # mssim = (mssim + 1) / 2
+    # mcs = (mcs + 1) / 2
 
-class MSSSIM(torch.nn.Module):
-    def __init__(self, window_size=11, size_average=True, channel=3):
-        super(MSSSIM, self).__init__()
-        self.window_size = window_size
-        self.size_average = size_average
-        self.channel = channel
+    pow1 = mcs ** weights
+    pow2 = mssim ** weights
+    # output = torch.prod(pow1 * pow2)
+    # From Matlab implementation https://ece.uwaterloo.ca/~z70wang/research/iwssim/
+    output = torch.prod(pow1[:-1] * pow2[-1])
+    return output
 
-    def forward(self, img1, img2):
-        # TODO: store window between calls if possible
-        return msssim(img1, img2, window_size=self.window_size, size_average=self.size_average)
